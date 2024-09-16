bda6e7618d87ffd516b0b6affc1e4ed427da3fd2
Vanilla Viking <ashwinr2k2@gmail.com>
2024-09-17 09:04:43 +1000

    feat: hsv filter

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,50 @@ impl Config {
         })
     }
 }
+
+pub fn apply_hsv_filter(img: &mut image::ImageBuffer<Rgb<u8>, Vec<u8>>, color_region: Vec<(f32, f32)>) {
+    img.enumerate_pixels_mut()
+        .for_each(|pixel| {
+            if is_in_range(pixel.2, &color_region) == false {
+                *pixel.2 = Rgb([0,0,0]);
+            }
+    });
+}
+
+// might benefit from caching
+pub fn is_in_range(rgb: &Rgb<u8>, color_region: &Vec<(f32, f32)>) -> bool {
+    let r = rgb[0] as f32 / 255.0;
+    let g = rgb[1] as f32 / 255.0;
+    let b = rgb[2] as f32 / 255.0;
+
+    let max = r.max(g).max(b);
+    let min = r.min(g).min(b);
+    let delta = max - min;
+
+    let h = if delta == 0.0 {
+        0.0
+    } else if max == r {
+        60.0 * (((g - b) / delta) % 6.0)
+    } else if max == g {
+        60.0 * (((b - r) / delta) + 2.0)
+    } else {
+        60.0 * (((r - g) / delta) + 4.0)
+    };
+
+    let hue = if h < 0.0 { h + 360.0 } else { h };
+    let saturation = if max == 0.0 { 0.0 } else { (delta / max) * 100.0};
+    let value = max * 100.0;
+
+    if hue < color_region[0].0 || hue > color_region[0].1 {
+        return false;
+    }
+    if saturation < color_region[1].0 || hue > color_region[1].1 {
+        return false;
+    }
+    if value < color_region[2].0 || hue > color_region[2].1 {
+        return false;
+    }
+
+    return true;
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{error::Error, io::Cursor};
-use firedetection_preprocessing::Config;
+use firedetection_preprocessing::{apply_hsv_filter, Config};
 use image::{ImageReader, Rgb};
 use std::{env, process};
 
@@ -13,7 +13,7 @@ fn main() {
         }
     };
 
-
-    let img = ImageReader::open(config.image_path.clone()).unwrap().decode().unwrap().to_rgb8();
-
+    let mut img = ImageReader::open(config.image_path.clone()).unwrap().decode().unwrap().to_rgb8();
+    apply_hsv_filter(&mut img, vec![(20.0, 40.0), (50.0, 255.0), (50.0, 255.0)]);
+    img.save("temp.png").unwrap();
 }
